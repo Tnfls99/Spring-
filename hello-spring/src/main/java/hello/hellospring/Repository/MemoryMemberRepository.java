2ebddc8c918e00375e9451d2d766ee7c8a9491ef
Surin Lee <leesurin99@gmail.com>
2022-07-02 00:02:25 +0900

    mvc & API 방식 예제 및 회원 관리 개발 예제 작성

---
+++ hello-spring/src/main/java/hello/hellospring/Repository/MemoryMemberRepository.java
@@ -0,0 +1,33 @@
+package hello.hellospring.Repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+    private static Map<Long, Member> store = new HashMap<>(); //key = member id, value = member
+    private static long sequence = 0L; // key generator
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long Id) {
+        return Optional.ofNullable(store.get(Id)); // Optional 사용해서 null이어도 감싸서 반환 가능
+    }
+
+    @Override
+    public Optional<Member> findMyName(String name) {
+        return store.values().stream() // java lambda -> 찾아보기~~~
+                .filter(member -> member.getName().equals(name)) // 슨환하면서 받아온 이름과 똑같은 객체만 반환된다.
+                .findAny(); // 이름이 같은 객체가 찾아지면 반환하고 종료
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<Member>(store.values()); // 데이터를 map으로 저장하니까 리스트 생성해서 돌려주기
+    }
+}
